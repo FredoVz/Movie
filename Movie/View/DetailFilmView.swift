74347162ff614e5c7619f23b7d9999d673bda480
FredoVz <dm.fredo018@gmail.com>
2021-12-23 12:56:47 +0700

    detail view + gambar di asset

---
+++ Movie/View/DetailFilmView.swift
@@ -0,0 +1,72 @@
+//
+//  DetailFilmView.swift
+//  Movie
+//
+//  Created by Wilfredo Sutanto on 23/12/21.
+//
+
+import SwiftUI
+
+struct DetailFilmView: View {
+    
+    @StateObject private var movieListVM = MovieListViewModel()
+    @State private var searchText: String = ""
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image("download").resizable().frame(width: UIScreen.main.bounds.width, height: 300)
+                
+                HStack {
+                    Image(systemName: "calendar").font(.system(size: 30))
+                    
+                    Text("22 Juli 2011").font(.system(size: 30))
+                Spacer()
+                }.padding()
+                
+                HStack {
+                    Image(systemName: "timer").font(.system(size: 30))
+                    
+                    Text("124 min").font(.system(size: 30))
+                    Spacer()
+                }.padding()
+                
+                HStack {
+                    Image(systemName: "star").font(.system(size: 30))
+                    
+                    Text("6.9").font(.system(size: 30))
+                    Spacer()
+                }.padding()
+                
+                Text("Steve Rogers, a rejected millitary soldier, transform into Captain America after taking a dose of a Super Soldier serum. But being Captain America comes at a price as he attempts to take down a war monger and a terrorist organization").padding()
+                
+                HStack {
+                    Text("Genre")
+                    
+                    Text("Action, Adventure, Sci-Fi")
+                    Spacer()
+                }.padding()
+                
+                HStack {
+                    Text("Director")
+                    
+                    Text("Joe Johnston")
+                    Spacer()
+                }.padding()
+                
+                HStack {
+                    Text("Actors")
+                    
+                    Text("Chris Evans, Hayley Atwell")
+                    Spacer()
+                }.padding()
+            }
+        }
+    }
+}
+
+struct DetailFilmView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailFilmView()
+    }
+}
